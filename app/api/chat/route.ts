28dad0082d742d8a94f929b59a7af791d9828c39
Mariana <mariana.olivandrade@gmail.com>
2024-09-02 10:51:34 -0300

    model version change

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,7 +7,7 @@ export async function POST (req: Request) {
     const {messages}: {messages: CoreMessage[]} = await req.json();
 
     const result = await streamText({
-        model: google("gemini-1.5-pro-latest"),
+        model: google("gemini-1.5-flash"),
         system: "You are a helpful assistant",
         messages,
     });
